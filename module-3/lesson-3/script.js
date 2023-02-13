a287ea3a65e4dd99dbafc440cec49d946fed45f9
mukhriddin-dev <khodievm@gmail.com>
2023-02-13 19:12:13 +0500

    ok

---
+++ module-3/lesson-3/script.js
@@ -0,0 +1,53 @@
+
+
+    /*document.write("Hello world");*/
+    // document.getElementById("text").innerText = "12+12";
+    // document.getElementById('id').innerHTML = "<li>JavaScript</li>";
+
+
+    //alert('saytga xush kelibsiz !')  // xabar
+    //confirm('saytga kirasizmi ?') // sayt sizga yoqdimi SO'ROV ?
+    //prompt('saytga haqida fikringiz ?') // sayt siz
+    // console.log('Is working on server') // message send to browser console
+    // console.warn('Warning !!!') // message send to browser console
+    // console.error('404 Not Found') // message send to browser console
+    // console.info('404 Not Found') // message send to browser console
+    // console.log({name:"Diyorbek", age:"19", job:"programmer and student"}) // message send to browser console
+    
+
+//  math operators
+//   + , - ,  / ,  * , % , ** ,    
+//  ++ INCREMENT
+//  -- DECREMENT
+// 
+//
+//
+//
+//
+
+//  console.log(12%13)
+
+// console.log(2**3) // 2*2*2
+
+// n=10;
+
+//console.log(++n)
+//console.log(n--)   
+// console.log(n)
+
+// document.getElementById('text').innerHTML="LOREM IPSUM"
+
+// * , / , % , ** , -,  "number"
+// + "concat"
+// 
+
+//1.  string -> "string" , 'string' , `string` -> 
+//2.  number -> 111, 0.1245534235 , 2.3 , -77326e237 
+//3.  boolean -> true , false , 0 yoki 1
+
+// console.log(true**true)
+
+
+
+
+
