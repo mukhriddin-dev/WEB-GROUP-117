a8c041ebd902eea791d4696d92179c3960dcf6d6
mukhriddin-dev <khodievm@gmail.com>
2023-02-13 20:22:54 +0500

    ok

---
+++ module-3/lesson-4/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// var numberOne = 1;
+// var NumberTwo = 2;
+// var number_two = 3;
+
+// camelCase
+// PascalCase
+// SnackeCase
+
+// one = "salom";
+//console.log(one); // undefined
+
+
+// var one = "Javascript";
+
+// console.log(one); //javascript
+// one = "PHP very Strong programming language";
+// var one = "Node.js very fast programming language";
+// var text="ok"
+
+// var text
+// console.log(text)
+
+// console.log(apple)
+
+// let apple = "aplle";
+// apple = "Olma"
+// // let apple = "samsung"
+// console.log(apple)
+
+
+// const device = "iPhone 14 pro lux max";
+    //   device = "Iphone 15 pro max lux"
+    // const device = "iPhone"
+    // device = device+"ok"
+    // console.log(device)
+
+
+// undefined
+
+
+// var a="js"
+//  a = null;
+// console.log(a)
+
+// let str1 =Symbol('salom')
+// let str2 =Symbol('salom')
+// console.log(str1==str2)
+
+
+
+
+
+
+
