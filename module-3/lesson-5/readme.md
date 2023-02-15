544ef0e7093698fec9440a4706c6aeab5741431e
mukhriddin-dev <khodievm@gmail.com>
2023-02-15 20:57:37 +0500

    lesson5

---
+++ module-3/lesson-5/readme.md
@@ -0,0 +1,8 @@
+### Day 5
+1. Template literal
+2. Conditional operator "if else"
+4. logical operators (&& and || or )
+5. Ternary operator
+6. "switch" statement
+
+3. Truthy and Falthy
---
+++ module-3/lesson-5/script.js
@@ -0,0 +1,121 @@
+"use strict";
+
+// alert("Saytga xush kelisbiz");
+
+// let isName = prompt('Ismingizni kiriting : ');
+// let age = prompt('Yoshingizni kiriting : ');
+// let tel = prompt('Telefon raqamingizni kiriting : ');
+
+// alert(`Asssalomu alaykum! ${isName}. Saytdan muaffaqiyatli ro'yhatdan o'tdingiz !` );
+
+// wrapper.innerHTML = `<ul>
+
+//          <li> <strong>Ism</strong>: ${isName} </li>
+//          <li> <strong>Yoshi</strong>: ${age} </li>
+//          <li> <strong>Tel:</strong>: ${tel} </li>
+
+//                     </ul>`;
+
+//                     console.log(" \"HTML\" ")
+
+// let power=prompt('Enter your device battery % ')
+
+// // if (power >= 10) {
+// //     wrapper.style.backgroundColor = "lime";
+// //     wrapper.innerHTML = `Your device battery is ${power}% `;
+
+// // } else {
+// //      wrapper.style.backgroundColor = "crimson";
+// //      wrapper.innerHTML = `Your device battery is ${power}% ! please charge low battery`;
+// // }
+
+// if (power % 2 == 0) {
+//     console.log("toq")
+// } else {
+//     console.log("son toq emas")
+// }
+
+// if else
+
+/* 
+
+
+IF ELSE CONDITIONAL OPERATOR
+
+if("sardor"=="Sardor"){
+   condition = true;
+}else{
+  condition = false;
+}
+
+*/
+
+let wrapper = document.getElementById("wrapper");
+
+// let dayNumber = +prompt("Hafta kunini kiriting:");
+
+// if (dayNumber === 1) {
+//   wrapper.innerText = "Dushanba";
+// } else if (dayNumber === 2) {
+//   wrapper.innerText = "Seshanba";
+// } else if (dayNumber === 3) {
+//   wrapper.innerText = "Chorshanba";
+// } else if (dayNumber === 4) {
+//   wrapper.innerText = "Payshanba";
+// } else if (dayNumber === 5) {
+//   wrapper.innerText = "Juma";
+// } else if (dayNumber === 6) {
+//   wrapper.innerText = "Shanba";
+// } else if (dayNumber === 7) {
+//   wrapper.innerText = "Seshanba";
+// } else {
+//   wrapper.innerText = `Haftaning ${dayNumber} - kuni  yo'q`;
+// }
+
+// let opertorNumber = +prompt("opertor number: ");
+
+// if (opertorNumber === 90 || opertorNumber === 91) {
+//     wrapper.innerText = "Beeline";
+//     console.log("Beeline");
+// } else if (opertorNumber === 93 || opertorNumber === 94) {
+//     console.log("Ucell");
+// } else if (
+//     opertorNumber === 77 ||
+//     opertorNumber === 97 ||
+//     opertorNumber === 88
+// ) {
+//     console.log("Mobi Uz");
+// } else {
+//     console.log("Operator number: Is no defined");
+// }
+
+// let a = prompt("a="),
+//   b = prompt("b="),
+//   c = prompt("c="),
+//   d = prompt("d=");
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//   console.log(`a=${a}, b=${b}, c=${c}, d=${d} sonlar musbat`);
+// } else {
+//   console.log("sonlardan biri yoki hammasi manfiy");
+// }
+
+// let score = prompt("Enter your score");
+// let ieltsScore = prompt("Enter your elts score");
+// let money = prompt("Enter your money . . .");
+
+// // 148 >
+// // level IELTS > 7.5
+// // 12000$
+
+// if ((score > 148 && ieltsScore > 7.5) || money > 11999) {
+//   console.log("You are Congratulations !!!");
+// } else {
+//   console.log("hard work  !!! try again and try again");
+// }
+
+
+// condition ? "true" : "false";
+
+
+// 10>9 ? ( 111>12 ? console.log("ok11") : console.log("fail11") ) : console.log('fail')
