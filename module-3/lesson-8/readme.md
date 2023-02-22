4d6dd0e5a102992671784d81b45dc27f74fe4808
mukhriddin-dev <khodievm@gmail.com>
2023-02-22 22:28:18 +0500

    ok

---
+++ module-3/lesson-8/readme.md
@@ -0,0 +1,34 @@
+### Day 7
+
+
+4. ## Function and function types
+5. Function arrow func / func expression / func declaration
+function parametrs
+6. Default parametr
+
+
+
+7. Array simple methods
+
+- ## Array
+- .push(4);
+- .pop();
+- .shift();
+- .unshift(0);
+- .concat('c');
+- .join('-');
+- .slice(1);
+- .indexOf('b');
+- .includes('c');
+
+- ## String methods
+- String.length
+- String.trim()
+- String.toUpperCase();
+- String.toLowerCase();
+- String.substring(x,y);
+- String.search();
+- String.trim();
+- String.match();
+- String.split();
+- charAt(n)
---
+++ module-3/lesson-8/script.js
@@ -0,0 +1,103 @@
+"use strict";
+
+
+// {
+//   var app = "telegram";
+//   // app = "whtasapp"
+//   var app = "facebook"
+// }
+
+// console.log(app)
+
+// {
+//   let app = "telegram";
+//   //   app = "whtasapp";
+//   // let app = "facebook";
+// //   console.log(app)
+// }
+// console.log(app);
+
+
+// const app = "telegram";
+// app = "whtasapp";
+// let app = "facebook";
+// console.log(app);
+
+// let app = null;
+
+
+// console.log(app)
+
+// 2142354 | null |
+
+
+// console.log(typeof 234234234n)
+
+// let str1 = Symbol('salom')
+// let str2 = Symbol('salom');
+
+
+// console.log(typeof str1)
+
+// if (str1 === str2) {
+//     console.log("xa")
+// } else {
+//     console.log("yo'q")
+// }
+
+
+// let a = 12;
+
+// let b = 13;
+
+
+// a /= b;
+
+
+// console.log(a)
+
+// console.log(a!=="12")
+
+// console.log(Math)
+
+// console.log(Math.max(-66, 44, 333, 2222))
+// console.log(Math.pow(2, 4)) // 2*2*2*2 -> 2**4   
+// console.log(Math.trunc(16.2434645645))
+// console.log(Math.pow(Math.trunc(Math.random() * 100),2));
+
+
+// console.log("12212" + true)
+// let num = "12";
+// let str = Number(num)
+// console.log(typeof str)
+// console.log(typeof null)
+// let isName = "Sardor";
+
+// console.log(`my name is ${ isName.substring(3) }`)
+
+// true -> 1 , "cvsrgerger" , true , " " , 
+// false -> NULL , 0 , undefined , "" , false  . . . . . .. 
+
+// if ("") {
+//     console.log("ok")
+// } else {
+//     console.log("error")
+// }
+    
+
+// if (a > b || a < c || a) {
+    
+// } else {
+    
+// }
+
+
+// 10>11 ? alert("ok") : alert("error")
+
+
+// switch ("ok") {
+//     case 1 : alert("ok"); break;
+//     case 2 : alert("ok"); break;
+//     case 3: alert("ok"); break;
+//     default : alert("error ???");
+// }
