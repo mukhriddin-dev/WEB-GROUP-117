9d88a78258017f4fc85e2b92ee2b6d000d711051
mukhriddin-dev <khodievm@gmail.com>
2023-03-08 20:51:43 +0500

    lesson 2 momnth 4

--- module-4/lesson-2/index.js
+++ module-4/lesson-2/index.js
@@ -102,22 +102,176 @@
 
 let wrapper = document.querySelector(".wrapper");
 
-for (let i = 0; i < 100; i++) {
-    let card = document.createElement("div");
-    card.setAttribute("id", "text");
-    card.classList.add("text-center", "bg-warning", "p-5", "rounded-5", "my-3");
-    card.innerHTML = `<div class="p-4 shadow">
-    
-    <img src="http://picsum.photos/id/${
-        i + 89
-    }/400/400" alt="image ${i} ">
-<strong>${
-        i + 1
-    }</strong> LOREM LOREM LOREM
+// for (let i = 0; i < 100; i++) {
+//     let card = document.createElement("div");
+//     card.setAttribute("id", "text");
+//     card.classList.add("text-center", "bg-warning", "p-5", "rounded-5", "my-3");
+//     card.innerHTML = `<div class="p-4 shadow">
 
-    </div>`;
+//     <img src="http://picsum.photos/id/${
+//         i + 89
+//     }/400/400" alt="image ${i} ">
+// <strong>${
+//         i + 1
+//     }</strong> LOREM LOREM LOREM
 
-    wrapper.prepend(card);
-}
+//     </div>`;
+
+//     wrapper.prepend(card);
+// }
 
 // console.log(card)
+
+
+let news = [
+
+    {
+        id: 1,
+        title: "lorem ipsum dolor sit ",
+        text: " lorem  lorem ipsum dolor sit lorem ipsum dolor sit lorem lore lorem ipsum dolor sit lorem ipsum dolor",
+        img: "https://picsum.photos/id/321/400/360"
+
+    },
+
+    {
+        id: 2,
+        title: "lorem ipsum dolor sit ",
+        text: " lorem  lorem ipsum dolor sit lorem ipsum dolor sit lorem lore lorem ipsum dolor sit lorem ipsum dolor",
+        img: "https://picsum.photos/id/421/400/360"
+
+    },
+
+
+    {
+        id: 3,
+        title: "lorem ipsum dolor sit ",
+        text: " lorem  lorem ipsum dolor sit lorem ipsum dolor sit lorem lore lorem ipsum dolor sit lorem ipsum dolor",
+        img: "https://picsum.photos/id/521/400/360"
+
+    },
+    {
+        id: 4,
+        title: "lorem ipsum dolor sit ",
+        text: " lorem  lorem ipsum dolor sit lorem ipsum dolor sit lorem lore lorem ipsum dolor sit lorem ipsum dolor",
+        img: "https://picsum.photos/id/121/400/360"
+
+    }, {
+        id: 5,
+        title: "lorem ipsum dolor sit ",
+        text: " lorem  lorem ipsum dolor sit lorem ipsum dolor sit lorem lore lorem ipsum dolor sit lorem ipsum dolor",
+        img: "https://picsum.photos/id/321/400/360"
+
+    }, {
+        id: 6,
+        title: "lorem ipsum dolor sit ",
+        text: " lorem  lorem ipsum dolor sit lorem ipsum dolor sit lorem lore lorem ipsum dolor sit lorem ipsum dolor",
+        img: "https://picsum.photos/id/721/400/360"
+
+    }
+
+
+];
+
+
+news.forEach((item, i) => {
+
+    let card = document.createElement('div');
+
+    card.setAttribute('class', 'd-flex shadow w-100 my-4');
+
+    card.innerHTML = `
+
+       <img src="${
+        item.img
+    }" alt="${
+        item.title
+    }"/>
+
+       <div class="card-body p-4">
+          <h2 class="fw-bold" >${
+        item.title
+    }</h2>
+
+          <p class="text-muted">
+             ${
+        item.text
+    }
+          </p>
+       </div>
+
+
+    `;
+
+
+    wrapper.appendChild(card);
+})
+
+
+let arr = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25
+];
+
+
+let newArr = [];
+
+
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+//     newArr.push(arr[i]+2)
+// }
+
+console.log(newArr)
+
+arr.forEach((index) => {
+    newArr.push(index * 2)
+})
+
+console.log(newArr)
+
+
+// function create() {
+//     let a = 10;
+
+//     return a*100
+// }
+
+// / pure function
+
+
+// impure function
+
+
+// let data = ['a', 'b', 'c', 'd', 'e'];
+
+// function impure(letter) {
+//     letter.push('f');
+
+//     return letter;
+// }
+
+
+// impure(data)
