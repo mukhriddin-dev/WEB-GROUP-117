736024bba7f9116916486be9b26067a2cb7e0082
mukhriddin-dev <khodievm@gmail.com>
2023-03-17 20:08:09 +0500

    part-1

--- module-4/lesson-5/index.js
+++ module-4/lesson-5/index.js
@@ -1,21 +1,7 @@
 "use strict";
 
-// {
-//     "title": "Patton Oswalt: Annihilation",
-//     "year": 2017,
-//     "categories": ["Uncategorized"],
-//     "imdbId": "tt7026230",
-//     "imdbRating": 7.4,
-//     "runtime": 66,
-//     "language": "English",
-//     "youtubeId": "4hZi5QaMBFc",
-//     "summary": "Patton Oswald, despite a personal tragedy, produces his best standup yet. Focusing on the tribulations of the Trump era and life after the loss of a loved one, Patton Oswald continues his journey to contribute joy to the world.",
-//     "smallThumbnail": "http://i3.ytimg.com/vi/4hZi5QaMBFc/hqdefault.jpg",
-//     "bigThumbnail": "http://i3.ytimg.com/vi/4hZi5QaMBFc/maxresdefault.jpg"
-// }
 
 movies.splice(50);
-
 
 
 // ============================= NORMALIZE ALL MOVIES =========================//
@@ -27,12 +13,18 @@ let allMovies = movies.map((item) => {
         category: item.categories,
         rating: item.imdbRating,
         lang: item.language,
-        link: `https://youtube.com/embed/${item.youtubeId}`,
+        link: `https://youtube.com/embed/${
+            item.youtubeId
+        }`,
         title: item.title,
         summary: item.summary,
         maxImg: item.bigThumbanil,
         minImg: item.smallThumbnail,
-        time: `${Math.trunc(item.runtime/60)}h ${item.runtime%60}m`
+        time: `${
+            Math.trunc(item.runtime / 60)
+        }h ${
+            item.runtime % 60
+        }m`
     };
 });
 
@@ -42,12 +34,19 @@ console.log(allMovies);
 // ============================= NORMALIZE ALL MOVIES END =========================//
 
 
-allMovies.forEach(item => {
-    const card = createElement('div', 'card', `
+function renderMovies(movies) {
+
+    if (movies && movies.length) {
+
+
+        movies.forEach(item => {
+            const card = createElement('div', 'card', `
        <div class="bg-image hover-overlay ripple"
                     data-mdb-ripple-color="light">
                     <img
-                      src="${item.minImg}"
+                      src="${
+                item.minImg
+            }"
                       class="img-fluid w-100" />
                     <a href="#!">
                       <div
@@ -56,15 +55,112 @@ allMovies.forEach(item => {
                     </a>
                   </div>
                   <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#!" class="btn btn-primary">Button</a>
+                    <h5 class="card-title">${
+                item.title
+            }</h5>
+                    <ul class="card-text">
+                     <li><strong> Language : </strong> ${
+                item.lang
+            }</li>
+                       <li><strong>Category: </strong> ${
+                item.category
+            }</li>
+                       <li><strong>Year: </strong> ${
+                item.year
+            }</li>
+                       <li><strong>Rating: </strong> ${
+                item.rating
+            }</li>
+                       <li><strong>Runtime: </strong> ${
+                item.time
+            }</li>
+                       <li><strong>YouTube: </strong> <a href="${
+                item.link
+            }" target="_blank">Watch</a> </li>
+                    </ul>
+                    <div class="d-flex gap-4"> 
+                         <a href="#!" class="btn btn-primary">Read more</a>
+                         <a href="#!" class="btn btn-warning">Add bookmark</a>
+                    </div>
                   </div>
     `);
 
-    $(".wrapper").append(card);
-});
+            $(".wrapper").append(card);
+        });
 
+
+    } else {
+        $(".wrapper").innerHTML = "<h1 class='text-center text-danger'> NOT FOUND </h1>";
+
+    }
+
+
+}
+
+renderMovies(allMovies);
+
+
+// ----------------------------- RENDER MOVIES END -----------------------------
+
+
+// ----------------------------- DYNAMIC CATEGORY -----------------------------
+
+
+function dynamicCategory(data) {
+
+    const categories = [];
+
+    if (data) {
+        data.forEach((item) => {
+            item.category.forEach(el => {
+                if (! categories.includes(el)) {
+                    categories.push(el);
+                }
+            })
+        })
+    }
+
+
+    if (categories.length) {
+        categories.forEach((item) => {
+            const option = createElement('option', 'list', item);
+            $('#category').append(option);
+        })
+    }
+}
+
+
+dynamicCategory(allMovies);
+
+// ----------------------------- DYNAMIC CATEGORY -----------------------------
+
+
+const findFilms = (filmtitle) => {
+
+    return allMovies.filter((item) => {
+        return item.title.includes(filmtitle);
+
+    })
+
+}
+
+
+$('#global-search').addEventListener('keyup', (e) => {
+
+    $(".wrapper").innerHTML = "";
+
+
+    const result = allMovies.filter((item) => {
+        return item.title.includes(e.target.value);
+    })
+
+
+    if (result.length) {
+        renderMovies(result)
+    } else {
+        $(".wrapper").innerHTML = `<h1 class='text-center text-warning'> SEARCH RESULT ${
+            result.length
+        } </h1>`
+    }
+
+})
