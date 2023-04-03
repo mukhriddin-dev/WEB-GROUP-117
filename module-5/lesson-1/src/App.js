d71787d2e073ab63339a1ab7a851fd799f03d203
mukhriddin-dev <khodievm@gmail.com>
2023-04-03 19:25:20 +0500

    hello reactjs

---
+++ module-5/lesson-1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function App () {
+  return (
+    <>
+      <h1 className='header'>Hello React js</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+        repellat voluptate? {new Date().getFullYear()}
+      </p>
+    </>
+  )
+}
+
+
+
